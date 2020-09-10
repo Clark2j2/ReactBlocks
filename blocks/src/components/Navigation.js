fe18585e25b654b7c9fe243b8425fe73e6bd3f7d
John Clark <clark2j2@johnmclark.com>
2020-09-10 14:00:59 -0400

    First Commit

---
+++ blocks/src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import styles from './Navigation.module.css'
+
+class Navigation extends Component {
+    render() {
+        return <div className={ styles.Navigation }></div>;
+    }
+}
+
+export default Navigation;
